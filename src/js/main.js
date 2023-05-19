4388f67b861743c6ff91c306f54de5019a28c975
Raphael Freire <raphaelmachadofreire@gmail.com>
2023-05-19 07:20:28 -0300

    deploy

--- src/js/main.js
+++ src/js/main.js
@@ -54,7 +54,6 @@ async function loadBackend () {
 
   // const resource = await fetch('http://localhost:3001/backend/backend.json').then(response => response.json())
   // return resource.stores
-  //
 }
 
 function getUniqueCitisFrom (backend) {
